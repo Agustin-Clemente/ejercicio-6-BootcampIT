0ab3b79a15048d5f6c1e4612743a1ae12346d37a
Agush <a_clemente@live.com.ar>
2023-07-22 19:30:35 -0300

    Punto B

---
+++ js/puntoB.js
@@ -0,0 +1,6 @@
+var cantidadDeGatos = 5;
+var cantidadDePasos = 3;
+
+for (let index = 1; index <= cantidadDeGatos; index++) {
+    console.log("Gato #" + index + ": 🐈" + "🐾".repeat(cantidadDePasos))
+}
